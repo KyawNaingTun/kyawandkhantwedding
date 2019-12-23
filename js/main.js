49b9bb817562793711f6e8f6327733af099dd7c0
Kyaw Naing Tun <kyawnainghtun93@gmail.com>
2019-12-23 16:51:12 +0630

    updated timezone

--- js/main.js
+++ js/main.js
@@ -310,7 +310,7 @@ AOS.init({
 
  	function makeTimer() {
 
- 		var endTime = new Date("26 December 2019 15:00:00 GMT+01:00");
+		  var endTime = new Date("26 December 2019 15:00:00 GMT+06:30");
  		endTime = (Date.parse(endTime) / 1000);
 
  		var now = new Date();
